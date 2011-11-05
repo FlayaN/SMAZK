e826272d8c01e2836710a7e3b46bb0bfc8cae381
Yngve <limmad_skinka@hotmail.com>
2011-11-05 19:05:40 +0100

    Forgot Particle class

---
+++ Particle.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include "Particle.hpp"
+#include <iostream>
+
+Particle::Particle(ParticleType particletype, sf::Image& image, float speed = 1.0f)
+    : MovingEntity(particletype, image, speed)
+{
+    sf::Randomizer random;
+    random.SetSeed(5);
+    duration = particletype.duration;// * (random.Random(particletype.duration-particletype.duration/2, particletype.duration+particletype.duration/2));
+
+    //std::cout << particletype.duration << " : " << duration << std::endl;
+
+}
+
+int Particle::getDuration()
+{
+    return duration;
+}
+
+void Particle::updateTimers(float elapsedTime)
+{
+    duration -= elapsedTime;
+    if (duration < 0) setDead();
+}
---
+++ Particle.hpp
@@ -0,0 +1,20 @@
+#include "MovingEntity.hpp"
+#include "ParticleType.hpp"
+#include "SFML/Graphics.hpp"
+#ifndef PARTICLEqqq
+#define PARTICLEqqq
+
+class Particle : public MovingEntity
+{
+public:
+    Particle(ParticleType particletype, sf::Image& image, float speed);
+
+    int getDuration();
+
+    void updateTimers(float elapsedTime);
+
+private:
+    float duration;
+};
+
+#endif
